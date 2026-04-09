455d04c5003631cdfd90d4883cc4714fb7e90051
Ricky Perez <36735566+RageKingBanana@users.noreply.github.com>
2026-04-09 23:08:25 +0800

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"
 // 1️⃣ Create a theme object
 const theme = extendTheme({}); // default theme
 
@@ -12,6 +13,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </ChakraProvider>
   </React.StrictMode>,
 );
